67750a31efd5b66fd4133dd1b7b333682887338b
vevomalik7766 <vevomalik7766@gmail.com>
2022-01-31 14:07:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Header, Footer } from './components/common'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<Header />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+Designing in Progress...
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<Footer />
+</div>
+);
 }
 
 export default App;
---
+++ src/components/common/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './Navbar.css'
+
+function Navbar () {
+
+  return (
+    <section className="navbar">
+      <a href="/" className="navbar-item">Home</a>
+      <a href="/howItWorks" className="navbar-item">How it Works</a>
+      <a href="/features" className="navbar-item">Features</a>
+      <a href="/myDocs" className="navbar-item">My Docs</a>
+      <a href="/blogs" className="navbar-item">Blogs</a>
+      <a href="/contact" className="navbar-item">Contact Us</a>
+      <a href="/signup" className="navbar-item">
+          <button className='navbar-item-btn'>
+              Signup
+          </button>
+      </a>
+      <a href="/signin" className="navbar-item">
+          <button className="navbar-item-btn">
+              Signin
+          </button>
+      </a>
+  </section>
+  )
+
+}
+
+export default Navbar;
